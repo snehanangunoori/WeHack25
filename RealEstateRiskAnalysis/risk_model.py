1bdbeccb2f2d4b352085a125a4e9375534eea0f6
Katy <soddykathryn@gmail.com>
2025-04-05 16:47:07 -0500

    debug

---
+++ RealEstateRiskAnalysis/risk_model.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load('risk_model.pkl')
+type_encoder = joblib.load('type_encoder.pkl')
+
+joblib.dump(model, 'risk_model.pkl')
+joblib.dump(type_encoder, 'type_encoder.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    investment_type = type_encoder.transform([data['investment_type']])[0]
+    return_rate = float(data['return_rate'])
+    years = int(data['years'])
+    has_history = 1 if data['has_history'] == 'yes' else 0
+
+    features = np.array([[investment_type, return_rate, years, has_history]])
+    pred = model.predict(features)[0]
+    label = 'risky' if pred == 1 else 'safe'
+    return jsonify({'prediction': label})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ run.sh
@@ -0,0 +1,15 @@
+#chmod a+x run.sh
+python3 -m pip --version
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+python3 -m pip install flask
+python3 -m pip show flask
+pip install flask
+source /path/to/venv/bin/activate
+pip install flask
+python3 -m pip install flask
+pip show flask
+pip install joblib
+source /Users/katy/Documents/GitHub/WeHack25/.venv/bin/activate
+
+python3 /Users/katy/Documents/GitHub/WeHack25/RealEstateRiskAnalysis/risk_model.py
